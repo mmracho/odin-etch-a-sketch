65dc2114b587bca2fe4b1f878d9486227f08da40
mmracho <20000699+mmracho@users.noreply.github.com>
2023-03-02 21:30:52 +0800

    Reorder buttons and options

--- js/etch.js
+++ js/etch.js
@@ -125,9 +125,9 @@ function randomRainbow() {
 
 function hexToRgbColor(hex) {
   const color = hex;
-  const r = parseInt(color.substr(1,2), 16)
-  const g = parseInt(color.substr(3,2), 16)
-  const b = parseInt(color.substr(5,2), 16)
+  const r = parseInt(color.substr(1, 2), 16)
+  const g = parseInt(color.substr(3, 2), 16)
+  const b = parseInt(color.substr(5, 2), 16)
   return `rgb(${r}, ${g}, ${b})`;
 }
 
